2acdc9f063938e3978dd8f74be165a728b7536d1
benbazaes <benjamin.bazaes@gmail.com>
2021-07-26 01:10:22 -0400

    se implementa la tarea de listas con mapas

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,17 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
+
+  const options = [{value:1, label:'Documento Credito'},{value:2, label:'Documento Pagos'},{value:3, label:'Documento Firmas'},{value:4, label:'Documento Morosos'}];
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      {options.map((x, key) => {
+        return(
+          <li key={x.value}>{x.label}</li>
+        );
+      })}
     </div>
   );
 }
